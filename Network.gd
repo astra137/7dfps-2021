1c625147aeee1947bac6c018679727cf215b8224
Astra <maccelerated@gmail.com>
2021-12-05 15:02:50 -0800

    Hotswap players
    Remove lobby logic from Network and add support for dedicated server.

--- Network.gd
+++ Network.gd
@@ -1,69 +1,72 @@
 extends Node
 
-const DEFAULT_PORT = 10567
-const MAX_PEERS = 10
+const DEFAULT_PORT := 10567
+const MAX_PEERS := 10
 
-signal player_list_changed()
-signal connection_failed()
-signal connection_succeeded()
-signal game_ended()
-signal game_error(what)
+signal disconnected()
 
 var world_scene = preload("res://Entities/World.tscn")
 var player_scene = preload("res://Player/Player.tscn")
 
-var player_list = []
 var world
+var peer
+var players = []
 
-puppet var players_ready = []
 
 func host_game(local_player: bool):
 	print("Starting server local_player=", local_player)
-	var peer = NetworkedMultiplayerENet.new()
+	peer = NetworkedMultiplayerENet.new()
 	var ok = peer.create_server(DEFAULT_PORT, MAX_PEERS)
-	if ok != OK: get_tree().quit(1);
-	get_tree().set_network_peer(peer)
+	if ok != OK: return get_tree().quit(1);
+	multiplayer.set_network_peer(peer)
 	world = world_scene.instance()
 	get_tree().get_root().add_child(world)
-	if local_player: player_add(1)
+	if local_player: _network_peer_connected(1)
+
 
 func join_game(address: String):
-	var peer = NetworkedMultiplayerENet.new()
+	peer = NetworkedMultiplayerENet.new()
 	var ok = peer.create_client(address, DEFAULT_PORT)
-	if ok != OK: get_tree().quit(1);
-	get_tree().set_network_peer(peer)
+	if ok != OK: return get_tree().quit(1);
+	multiplayer.set_network_peer(peer)
 	world = world_scene.instance()
 	get_tree().get_root().add_child(world)
 
+
 func quit_game():
-	get_tree().set_network_peer(null)
+	multiplayer.set_network_peer(null)
 	if has_node("/root/World"):
 		get_node("/root/World").queue_free()
-	players_ready.clear()
-	player_list.clear()
-	emit_signal("game_ended")
+	players.clear()
+	emit_signal("disconnected")
 
-func player_add(player_id: int):
-	print("player_add ", player_id)
-	player_list.append(player_id)
-	var player = player_scene.instance()
+
+remotesync func player_join(player_id: int):
+	var sender_id = multiplayer.get_rpc_sender_id()
+	assert(sender_id == 1)
+	print("player_join ", player_id)
+	var player: KinematicBody = player_scene.instance()
 	player.set_name(str(player_id))
 	player.set_network_master(player_id)
+	player.set_process(false)
 	world.get_node("Players").add_child(player)
-	emit_signal("player_list_changed")
 
-func player_remove(player_id: int):
-	print("player_remove ", player_id)
-	players_ready.erase(player_id)
+
+remote func player_start(player_id: int):
+	var sender_id = multiplayer.get_rpc_sender_id()
+	assert(sender_id == 1)
+	print("player_start ", player_id)
+	var player: KinematicBody = world.get_node("Players").get_node(str(player_id))
+	player.set_process(true)
+
+
+remotesync func player_leave(player_id: int):
+	var sender_id = multiplayer.get_rpc_sender_id()
+	assert(sender_id == 1)
+	print("player_leave ", player_id)
 	var player: KinematicBody = world.get_node("Players").get_node(str(player_id))
 	player.queue_free()
-	emit_signal("player_list_changed")
 
-#
-#
-#
-#
-#
 
 func _ready():
 	# warning-ignore:return_value_discarded
@@ -81,64 +84,31 @@ func _ready():
 func _network_peer_connected(id):
 	print("_network_peer_connected ", id)
 	if multiplayer.is_network_server():
-		for player_id in player_list:
+		for player_id in players:
 			rpc_id(id, "player_join", player_id)
+		players.append(id)
+		rpc("player_join", id)
+		rpc_id(id, "player_start")
 
-		player_add(id)
-		for peer_id in multiplayer.get_network_connected_peers():
-			rpc_id(peer_id, "player_join", id)
-		# TODO: send server if server wants to play
 
 func _network_peer_disconnected(id):
 	print("_network_peer_disconnected", id)
 	if multiplayer.is_network_server():
-		player_remove(id)
-		for peer_id in multiplayer.get_network_connected_peers():
-			rpc_id(peer_id, "player_leave", id)
-		# Server physically can't be disconnected
+		players.erase(id)
+		rpc("player_leave", id)
+
 
 func _connected_to_server():
 	print("_connected_to_server")
-	emit_signal("connection_succeeded")
+
 
 func _connection_failed():
 	print("_connection_failed")
 	get_tree().set_network_peer(null)
-	emit_signal("connection_failed")
+	emit_signal("disconnected")
+
 
 func _server_disconnected():
 	print("_server_disconnected")
 	quit_game()
-	emit_signal("game_error", "Server disconnected")
-
-
-#
-#
-#
-#
-
-
-# Server -> all clients notification of player change
-remote func player_join(player_id: int):
-	var sender_id = multiplayer.get_rpc_sender_id()
-	assert(sender_id <= 1)
-	player_add(player_id)
-
-
-# Server -> all clients notification of player change
-remote func player_leave(player_id: int):
-	var sender_id = multiplayer.get_rpc_sender_id()
-	assert(sender_id == 1)
-	player_remove(player_id)
-
-
-# Self client -> server notification that client is ready
-remote func set_player_ready(flag: bool):
-	assert(multiplayer.is_network_server())
-	var sender_id = multiplayer.get_rpc_sender_id()
-	if flag:
-		if not players_ready.find(sender_id):
-			players_ready.append(sender_id)
-	else:
-		players_ready.erase(sender_id)
-	rset("players_ready", players_ready)
+	emit_signal("disconnected")
--- UI/Lobby.gd
+++ UI/Lobby.gd
@@ -2,39 +2,25 @@ extends Control
 
 func _ready():
 	# warning-ignore:return_value_discarded
-	Network.connect("connection_failed", self, "_on_connection_failed")
-	# warning-ignore:return_value_discarded
-	Network.connect("connection_succeeded", self, "_on_connection_success")
-	# warning-ignore:return_value_discarded
-	Network.connect("player_list_changed", self, "refresh_lobby")
-	# warning-ignore:return_value_discarded
-	Network.connect("game_ended", self, "_on_game_ended")
-	# warning-ignore:return_value_discarded
-	Network.connect("game_error", self, "_on_game_error")
+	Network.connect("disconnected", self, "_on_disconnected")
 
 func _on_join_pressed():
-	Network.join_game($Menu/Address.text)
+	var address = $Menu/Address.text
+	if address.empty(): address = "localhost"
+	Network.join_game(address)
 	$Menu.hide()
 
 func _on_start_pressed():
 	Network.host_game(true)
 	$Menu.hide()
 
-func refresh_lobby():
-	print("refresh_lobby")
+func _on_start_pressed2():
+	Network.host_game(false)
+	$Menu.hide()
 
-func _on_connection_success():
-	print("_on_connection_success")
-
-func _on_connection_failed():
-	print("_on_connection_failed")
+func _on_disconnected(errtxt: String):
+	print("Lobby _server_disconnected")
 	$Menu.show()
-
-func _on_game_ended():
-	print("_on_game_ended")
-	$Menu.show()
-
-func _on_game_error(errtxt):
-	$Menu.show()
-	$ErrorDialog.dialog_text = errtxt
-	$ErrorDialog.popup_centered_minsize()
+	if not errtxt.empty():
+		$ErrorDialog.dialog_text = errtxt
+		$ErrorDialog.popup_centered_minsize()
